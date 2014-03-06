cdc36c9d079e5691fe8ddc3b9dd9b82a6fd1eb97
Akash Devaraju <akashdevaraju@gmail.com>
2014-03-07 00:44:47 +0530

    [Projects] Updated fetch/insert method

--- gitlab-milestone.rb
+++ gitlab-milestone.rb
@@ -5,14 +5,13 @@ require 'redis'
 g = Gitlab.client(:endpoint => 'http://code.icicletech.com/api/v3/', :private_token => 'KMpLRJE8mzhuGsBv1xus')
 
 # Get all projects for the user
-my_projects = g.projects
+my_projects = g.projects.map { |project| {id: project.id, name: project.name, description: project.description}}
 
 # get ids for all the projects
-user_proj_ids = []
-my_projects.each.map {|project| user_proj_ids <<  project.id}
+proj_ids = []
+my_projects.each {|project| proj_ids <<  project[:id]}
 
 # Get a particular milestone
-
 milestones_for_project = g.milestones(27).map {|milestone| {id: milestone.id, title: milestone.title}}
 milestone_ids = []
 milestones_for_project.each {|milestone| milestone_ids << milestone[:id]}

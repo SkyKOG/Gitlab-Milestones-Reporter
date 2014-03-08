d2ba0b72ff80af716ee9f2c04c1585e1da4e8081
Akash Devaraju <akashdevaraju@gmail.com>
2014-03-08 16:37:30 +0530

    [Issues] Fetch issues for each project belonging to a project milestone

--- gitlab-milestone.rb
+++ gitlab-milestone.rb
@@ -21,8 +21,19 @@ projects_having_milestones = []
 milestones_for_projects.each { |mile_proj| projects_having_milestones << mile_proj.first[:project_id] }
 
 
-# milestone_ids = []
-# milestones_for_project.each {|milestone| milestone_ids << milestone[:id]}
+# fetch issues for each project belonging to a project milestone
+all_issues = []
+
+projects_having_milestones.each do |project|
+  i = 1
+  loop do
+    issues_in_this_page = g.issues(project,{:page=>i,:per_page=>100})
+    break if issues_in_this_page.empty?
+    issues_in_this_page.reject! { |c| c.milestone.nil? }
+    issues_in_this_page.map {|issue_in_this_page| all_issues << {issue_id: issue_in_this_page.id, issue_title: issue_in_this_page.title, issue_state: issue_in_this_page.state, project_id: issue_in_this_page.project_id, milestone_id: issue_in_this_page.milestone.id }}
+    i+=1
+  end
+end
 
 
 
@@ -31,3 +42,20 @@ milestones_for_projects.each { |mile_proj| projects_having_milestones << mile_pr
 g.issues(27,{:page=>1,:per_page=>100}).count
 
 proj_ids.each {|proj_id| milestones_for_projects << g.milestones(proj_id).map {|milestone| {id: milestone.id, title: milestone.title, project_id: milestone.project_id}}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# milestone_ids = []
+# milestones_for_project.each {|milestone| milestone_ids << milestone[:id]}

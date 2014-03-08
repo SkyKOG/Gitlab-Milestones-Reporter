c2e46060aafbebedc0fed3a0814053c3b24d4f53
Akash Devaraju <akashdevaraju@gmail.com>
2014-03-08 23:11:50 +0530

    [Table] Output table to file

--- gitlab-milestone.rb
+++ gitlab-milestone.rb
@@ -2,6 +2,7 @@ require 'json'
 require 'gitlab'
 require 'redis'
 require 'formatador'
+require 'active_support/core_ext/integer/inflections'
 
 # initialise the gitlab client to access gitlab api
 g = Gitlab.client(:endpoint => 'http://code.icicletech.com/api/v3/', :private_token => 'KMpLRJE8mzhuGsBv1xus')
@@ -35,8 +36,36 @@ my_projects.keys.each do |project|
   end
 end
 
-my_projects.keys.each do |project|
-  puts my_projects[project][:name]
-  milestone_status_hash = my_projects[project][:milestones].keys.map { |k| { milestone: my_projects[project][:milestones][k][:title], total: my_projects[project][:milestones][k][:count], completed: my_projects[project][:milestones][k][:finished], percentage: my_projects[project][:milestones][k][:finished].to_s + "%", remaining: my_projects[project][:milestones][k][:count] - my_projects[project][:milestones][k][:finished]}}
-  Formatador.display_table(milestone_status_hash, [:milestone, :percentage, :total, :completed, :remaining])
+# display output in table
+File.open('gitlab-milestones.html', 'w') do |gmile|
+  gmile.puts "<h1>" + "Gitlab Milestone Report " + "</h1>"
+  gmile.puts "<h3>" + "Account: " + g.user.name.to_s + "</h3>"
+  gmile.puts "<h3>" + "Time: " + Time.new.strftime("%a, %b #{time.day.ordinalize} %Y, %I:%M %p") + "</h3>"
+
+  gmile.puts "<hr />"
+  my_projects.keys.each do |project|
+    puts my_projects[project][:name]
+    milestone_status_hash = my_projects[project][:milestones].keys.map { |k| { milestone: my_projects[project][:milestones][k][:title], total: my_projects[project][:milestones][k][:count], completed: my_projects[project][:milestones][k][:finished], percentage: my_projects[project][:milestones][k][:finished].to_s + "%", remaining: my_projects[project][:milestones][k][:count] - my_projects[project][:milestones][k][:finished]}}
+    Formatador.display_table(milestone_status_hash, [:milestone, :percentage, :total, :completed, :remaining])
+
+    gmile.puts "<h2>" + my_projects[project][:name] + "</h2>"
+    gmile.puts '<table border="1">'
+      gmile.puts "<tr>"
+        gmile.puts "<th>" + "Milestone"+"</th>"
+        gmile.puts "<th>" + "Percentage"+"</th>"
+        gmile.puts "<th>" + "Total"+"</th>"
+        gmile.puts "<th>" + "Completed"+"</th>"
+        gmile.puts "<th>" + "Remaining"+"</th>"
+      gmile.puts "</tr>"
+      my_projects[project][:milestones].keys.each do |t|
+        gmile.puts "<tr>"
+        gmile.puts "<td>" + my_projects[project][:milestones][t][:title].to_s + "</td>"
+        gmile.puts "<td>" + my_projects[project][:milestones][t][:percentage].to_i.to_s + "%"+ "</td>"
+        gmile.puts "<td>" + my_projects[project][:milestones][t][:finished].to_s + "</td>"
+        gmile.puts "<td>" + my_projects[project][:milestones][t][:count].to_s + "</td>"
+        gmile.puts "<td>" + (my_projects[project][:milestones][t][:count] - my_projects[project][:milestones][t][:finished]).to_s + "</td>"
+        gmile.puts "</tr>"
+      end
+    gmile.puts "<table>"
+  end
 end
